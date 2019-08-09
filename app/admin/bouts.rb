84a8d598566e4d475f015eba309795ca478367a2
Raghu Betina <raghu@firstdraft.com>
2019-08-09 16:50:50 +0000

    Whitelist columns in Bout for ActiveAdmin

--- app/admin/bouts.rb
+++ app/admin/bouts.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Bout do
+
+ permit_params :score_1, :score_2, :score_3, :score_4, :score_5, :team_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
