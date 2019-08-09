403ff662db000a96a8b0a4ef90a8439eb14d2d72
Raghu Betina <raghu@firstdraft.com>
2019-08-09 16:50:21 +0000

    Whitelist columns in Coach for ActiveAdmin

--- app/admin/coaches.rb
+++ app/admin/coaches.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Coach do
+
+ permit_params :first_name, :last_name, :team_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
